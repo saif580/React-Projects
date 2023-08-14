1c6a99e1a73634f7093ce8ac452c693ea911697a
saif580 <saifulhasan580@gmail.com>
2023-08-14 17:33:38 +0530

    imperative hook

--- Advance/src/components/Home/Home.js
+++ Advance/src/components/Home/Home.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React,{useContext} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
+import AuthContext from '../store/auth-context';
 
 const Home = (props) => {
+  const authctx=useContext(AuthContext)
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+      <button onClick={authctx.onLogout}>Logout</button>
     </Card>
   );
 };
