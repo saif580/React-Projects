614e1efa58312464018ab7eecb25464e096dfd14
saif580 <saifulhasan580@gmail.com>
2023-08-24 17:35:56 +0530

    post

--- connectingToDB/src/App.js
+++ connectingToDB/src/App.js
@@ -19,7 +19,6 @@ function App() {
       }
 
       const data = await response.json();
-      console.log(data);
       const loadedMovies=[];
       for(const key in data){
         loadedMovies.push({
@@ -58,7 +57,6 @@ function App() {
         }
       });
       const data=await response.json();
-      console.log(data);
   }
 
   let content = <p>Found no movies.</p>;
