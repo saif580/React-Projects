d678a1f81557132818e35c93555556f1f65b9049
saif580 <saifulhasan580@gmail.com>
2023-08-28 16:17:33 +0530

    usememo

---
+++ custom hooks 1/src/App.js
@@ -0,0 +1,51 @@
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const transformTasks = useCallback((tasksObj) => {
+    const loadedTasks = [];
+
+    for (const taskKey in tasksObj) {
+      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+    }
+
+    setTasks(loadedTasks);
+  }, []);
+  const data = useMemo(() => {
+    return {
+      url: "https://react-http-c23d8-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+    };
+  }, []);
+
+  const {
+    isLoading,
+    error,
+    sendRequest: fetchTasks,
+  } = useHttp(data, transformTasks);
+
+  useEffect(() => {
+    fetchTasks();
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
