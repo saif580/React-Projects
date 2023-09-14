326e6980f20c989973877f812c05ce8ac260cce6
saif580 <saifulhasan580@gmail.com>
2023-09-14 20:12:33 +0530

    contact app

---
+++ RouterExampleProject/contact_List/src/routes/index.jsx
@@ -0,0 +1,13 @@
+export default function Index() {
+    return (
+      <p id="zero-state">
+        This is a demo for React Router App.
+        <br />
+        Check out{" "}
+        <a href="https://reactrouter.com">
+          Dummy contact app
+        </a>
+        .
+      </p>
+    );
+  }
